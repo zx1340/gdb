b70b82b1947d01ba1155b8dfb6ff75360f0118c8
zx1340 <phambao@outlook.com>
2016-11-10 14:53:18 +0700

    Update scan.py

--- scan.py
+++ scan.py
@@ -9,19 +9,21 @@ def get_all_input():
 def write_gdb(bp,arg):
     f = open('cmd.gdb','w')
     f.write('b* '+bp + '\nr ' + arg)
+    f.close()
 
-bp = 'b'
+#give breakpoint location
+breakpoint = 'b'  #function b is our example
 
 all_input = get_all_input()
 
 cmd = ['gdb', '--batch','--command=cmd.gdb','--args','core']
 for i in all_input:
     
-    write_gdb(bp,i)
+    write_gdb(breakpoint,i)
     print '[+] Scanning file',i
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE,stderr=subprocess.PIPE,stdin=subprocess.PIPE)
     out, err = p.communicate()
     #print out,err
     if 'Breakpoint 1,' in out:
         print "--------------------------- FOUNDED -----------------------",i
-        #break
+        break
